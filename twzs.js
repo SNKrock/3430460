2f846705ae0d7da76e6f6b7ef0d39709d699f60b
SNKrock <123874474+SNKrock@users.noreply.github.com>
2024-05-02 17:47:28 +0800

    Update twzs.js

--- twzs.js
+++ twzs.js
@@ -1,7 +1,8 @@
 /*******************************
-脚本名称:  
+脚本名称:  推文助手
+描述:推文助手
 脚本作者：彭于晏💞
-更新时间：2024年5月2日 下午5:27
+更新时间：2024年5月2日 下午5:46
 问题反馈：QQ+89996462
 QQ会员群：779392027
 TG反馈群：https://t.me/plus8889
@@ -11,23 +12,22 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^http[s]?:\/\/(league-api.huazhuanapp.com|league-api.huazhuanapp.com)(/api/v1/user/permanentPoints|/api/v1/wallpaper/generator/vip/info) url script-response-body https://raw.githubusercontent.com/SNKrock/3430460/main/twzs.js
+^http[s]?:\/\/league-api.huazhuanapp.com\/api\/v1\/wallpaper\/generator\/vip\/info url script-response-body https://raw.githubusercontent.com/SNKrock/3430460/main/twzs.js
 
 [mitm] 
 
-hostname = (league-api.huazhuanapp.com|league-api.huazhuanapp.com)
+hostname = league-api.huazhuanapp.com
 
 *******************************/
 
-var body = $response.body;
-var url = $request.url;
-
-if (url.indexOf("/api/v1/user/permanentPoints") != -1) {
-  body = JSON.stringify("vipPoints":999999999);
-}
-
-if (url.indexOf("/api/v1/wallpaper/generator/vip/info") != -1) {
-  body = JSON.stringify("showVipAlert":true);
-}
-
+var body = $response.body.replace(/"vipPoints":\d+/g,'"vipPoints":999999999')
+.replace(/"showVipAlert":\w+/g,'"showVipAlert":true')
+.replace(/"vipDesc":".*?"/g,'"vipDesc":"会员有效期至：2999-09-09 19:19"')
+.replace(/"mobile":".*?"/g,'"mobile":"139****8888(年卡会员)"')
+.replace(/"coins":\d+/g,'"coins":99999999999')
+.replace(/"vipDesc":".*?"/g,'"vipDesc":"会员有效期至：2099-09-09 14:38"')
+.replace(/"mobile":".*?"/g,'"mobile":"139****9999(年卡会员)"')
+.replace(/"coins":\d+/g,'"coins":99999999')
+.replace(/"showVipAlert":\w+/g,'"showVipAlert":true')
+.replace(/"vipStatus":\d/g,'"vipStatus":1')
 $done({ body });
